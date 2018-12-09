f0675b8944b7169f52149598b3cd7546fc85f7ae
Mona Aliasgari <m_aliasgari@rasana.ir>
2018-12-09 11:11:26 +0330

    Updates

---
+++ precache-manifest.f447199ca7bcd2a75fad631ea9ac61e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db182d43c69b64e1c48b",
+    "url": "/resume/static/css/main.21b1f9f5.chunk.css"
+  },
+  {
+    "revision": "db182d43c69b64e1c48b",
+    "url": "/resume/static/js/main.db182d43.chunk.js"
+  },
+  {
+    "revision": "9a434c6bf3b0ad5957d0",
+    "url": "/resume/static/js/1.9a434c6b.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "b2519bc46f0bcd913c594996783dd1db",
+    "url": "/resume/index.html"
+  }
+];
